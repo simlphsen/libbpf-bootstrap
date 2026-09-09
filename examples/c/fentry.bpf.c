f046d75c60b59178a9a2c445ab298933bcaa4628
Simlphsen <62537427+simlphsen@users.noreply.github.com>
2026-09-09 15:34:48 +0800

    Add kprobes for fuse_create_open and fuse_atomic_open

--- examples/c/fentry.bpf.c
+++ examples/c/fentry.bpf.c
@@ -3,9 +3,11 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
+/*
 SEC("fentry/do_unlinkat")
 int BPF_PROG(do_unlinkat, int dfd, struct filename *name)
 {
@@ -25,3 +27,28 @@ int BPF_PROG(do_unlinkat_exit, int dfd, struct filename *name, long ret)
 	bpf_printk("fexit: pid = %d, filename = %s, ret = %ld\n", pid, name->name, ret);
 	return 0;
 }
+*/
+
+SEC("kprobe/fuse_create_open")
+int BPF_KPROBE(fuse_create_open, struct inode *dir, struct dentry *entry)
+{
+	if (bpf_get_current_uid_gid() != 0) return;
+
+	void* ptr = BPF_CORE_READ(entry, d_u.d_alias.pprev);
+	
+	bpf_printk("xxx fuse_create_open: ptr = %lx, ino = %lu\n", ptr, BPF_CORE_READ(dir, i_ino));
+	return 0;
+}
+
+SEC("kprobe/fuse_atomic_open")
+int BPF_KPROBE(fuse_atomic_open, struct inode *dir, struct dentry *entry)
+{
+	if (bpf_get_current_uid_gid() != 0) return;
+
+	void* ptr = BPF_CORE_READ(entry, d_u.d_alias.pprev);
+	
+	bpf_printk("xxx fuse_atomic_open: ptr = %lx, ino = %lu\n", ptr, BPF_CORE_READ(dir, i_ino));
+	return 0;
+}
+
+
